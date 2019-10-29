680b955fac5a142a272c30302a35b6b9440c2a6b
Gerson Montenegro <montenegrogerson@gmail.com>
2019-10-29 00:59:07 -0300

    refactor: allowing uppercase extension

--- src/assets/createvars.js
+++ src/assets/createvars.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const vPath = 'img/vocabulary';
 const iPath = 'img/icons';
+const fPath = 'fonts';
 const fileName = 'index.js';
 const fileHeader = 'const Assets = {';
 const fileFooter = '};\rexport default Assets;\r';
@@ -8,17 +9,21 @@ const fileFooter = '};\rexport default Assets;\r';
 const getFiles = path => {
   return fs
     .readdirSync(path)
-    .filter(file => file.endsWith('.png'))
+    .filter(file => file.endsWith('.png') || file.endsWith('.ttf') || file.endsWith('.TTF'))
     .map(file => {
       const propName = file
         .split('/')
         .toString()
         .split('.')[0];
-      return `  ${propName}: require('assets/${path}/${file}')`;
+        console.log('looking for a font: ', file);
+      if (file.endsWith('.png')) {
+        return `  ${propName}: require('assets/${path}/${file}')`;
+      }
+      return `  ${propName}: '${propName}'`;
     });
 };
 
-const imgFiles = [...getFiles(vPath), ...getFiles(iPath)];
+const imgFiles = [...getFiles(vPath), ...getFiles(iPath), ...getFiles(fPath)];
 const createFile = (content, header, footer) => {
   let fileContent = header;
   content.map(file => (fileContent = `${fileContent}\n${file},`));
