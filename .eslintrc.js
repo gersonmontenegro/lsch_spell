0f01f0ff67a33782939e7aa8c1a9da96ab0405cf
Gerson Montenegro <montenegrogerson@gmail.com>
2019-10-29 21:55:14 -0300

    refactor: change lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,30 @@
-module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+{
+  "parser": "babel-eslint",
+  "plugins": [
+      "react",
+      "react-native",
+      "react-hooks"
+  ],
+  "parserOptions": {
+      "ecmaFeatures": {
+          "ecmaVersion": 6,
+          "jsx": true,
+          "modules": true
+      }
+  },
+  "extends": [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "airbnb"
+  ],
+  "rules": {
+      "indent": [
+          "error",
+          4,
+          {
+              "SwitchCase": 1
+          }
+      ],
+      "import/no-named-as-default": "off",
+  },
+}
