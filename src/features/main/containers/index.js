196d5e60021c1161d70df27bd435451f8655e908
Gerson Montenegro <montenegrogerson@gmail.com>
2019-10-29 21:56:23 -0300

    chore: implementing main components

--- src/features/main/containers/index.js
+++ src/features/main/containers/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import TopBar from '../components/TopBar';
+import Hands from '../components/Hands';
+import Footer from '../components/Footer';
 
 const Main = () => {
     return (
         <View style={{flex: 1}}>
-            <View style={{height: 30}}>
-                <TopBar />
-            </View>
-            <View style={{flex: 3, backgroundColor: 'blue'}}>
-                <Text>Hands...</Text>
-            </View>
-            <View style={{flex: 1, backgroundColor: 'red'}}>
-                <Text>Footer...</Text>
-            </View>
+            <TopBar />
+            <Hands />
+            <Footer />
         </View>
     );
 };
