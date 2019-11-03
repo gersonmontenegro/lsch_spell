13f13dca4a77a4976c461b53ca228a0a16fd9daf
Gerson Montenegro <montenegrogerson@gmail.com>
2019-11-03 17:17:25 -0300

    chore: controlling letters passing throught time

--- src/features/main/components/HandsSpell.js
+++ src/features/main/components/HandsSpell.js
@@ -1,21 +1,30 @@
 import React, { useEffect, useCallback, useMemo, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Image, Text } from 'react-native';
 import useInterval from 'use-interval';
 import Assets from 'assets';
 import Styles from '../../../styles';
 import useFetch from '../../../providers/hooks';
+import { selectCurrentWord } from 'reducers/selectors';
+import { setWord } from 'actions';
 
 const urlDemoData = 'https://raw.githubusercontent.com/gersonmontenegro/lsch_spell/master/src/providers/demoText.txt';
 
 const HandsSpell = (props) => {
-    let [ count, setCount ] = useState(0);
+    const dispatch = useDispatch();
+    const currentWord = useSelector(selectCurrentWord);
+    let [ index, setIndex ] = useState(0);
     let [ currentLetter , setCurrentLetter ] = useState('');
+    const { word } = props;
     useInterval(() => {
-        const { word } = props;
-        setCount(count + 1);
-        setCurrentLetter(Assets[word[count].toLowerCase()]);
-        if (count >= word.length - 1) {
-            setCount(0);
+        if (index === word.length) {
+            setIndex(0);
+            dispatch(setWord({letter: '', index}));
+        } else {
+            const letter = word[index].toLowerCase();
+            setCurrentLetter(Assets[letter]);
+            dispatch(setWord({letter: word[index].toLowerCase(), index}));
+            setIndex(index + 1);
         }
     }, 1000);
     return (
