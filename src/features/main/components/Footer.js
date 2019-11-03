9278c3efc37667a690a1e50617204217171773c0
Gerson Montenegro <montenegrogerson@gmail.com>
2019-11-03 17:18:04 -0300

    chore: implementing selectors to get current word

--- src/features/main/components/Footer.js
+++ src/features/main/components/Footer.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { View, Text } from 'react-native';
 import Assets from 'assets';
 import Styles from '../../../styles';
+import { getWord } from 'actions';
+import { selectCurrentWord } from 'reducers/selectors';
 
 const Footer = () => {
+    const currentWord = useSelector(selectCurrentWord);
     return (
         <View style={Styles.FooterContainerStyle}>
-            <Text style={Styles.FooterMainTextStyle}>OOOOOOOOOOOOO</Text>
+            <Text style={Styles.FooterMainTextStyle}>{currentWord}</Text>
         </View>
     );
 };
