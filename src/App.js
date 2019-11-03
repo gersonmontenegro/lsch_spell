b6d857f6c5af869bfadedb36deacb3478935303f
Gerson Montenegro <montenegrogerson@gmail.com>
2019-11-03 17:17:40 -0300

    chore: implementing provider

--- src/App.js
+++ src/App.js
@@ -10,16 +10,18 @@ import {
   Image,
 } from 'react-native';
 import Assets from './assets';
+import { Provider } from 'react-redux';
+import store from 'store';
 import Main from './features/main/containers';
 import Styles from './styles';
 
 const App: () => React$Node = () => {
   return (
-    <>
+    <Provider store={store}>
       <SafeAreaView style={Styles.MainContainer}>
         <Main />
       </SafeAreaView>
-    </>
+    </Provider>
   );
 };
 
