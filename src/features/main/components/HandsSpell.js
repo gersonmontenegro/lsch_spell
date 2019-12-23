9a04341e297442becf722a6603cf132b3c407394
Gerson <gersonm@mobilecorp.co>
2019-12-23 04:18:08 -0300

    chore: clean component

--- src/features/main/components/HandsSpell.js
+++ src/features/main/components/HandsSpell.js
@@ -1,33 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Image } from 'react-native';
-import useInterval from 'use-interval';
 import Assets from 'assets';
-import selectCurrentWord from 'reducers/selectors';
-import { setWord } from 'actions';
 import Styles from '../../../styles';
 
-const urlDemoData = 'https://raw.githubusercontent.com/gersonmontenegro/lsch_spell/master/src/providers/demoText.txt';
-
-const HandsSpell = (props) => {
-    const dispatch = useDispatch();
-    const currentWord = useSelector(selectCurrentWord);
-    let [ index, setIndex ] = useState(0);
-    let [ currentLetter , setCurrentLetter ] = useState('');
-    const { word } = props;
-    useInterval(() => {
-        if (index === word.length) {
-            setIndex(0);
-            dispatch(setWord({letter: '', index}));
-        } else {
-            const letter = word[index].toLowerCase();
-            setCurrentLetter(Assets[letter]);
-            dispatch(setWord({letter: word[index].toLowerCase(), index}));
-            setIndex(index + 1);
-        }
-    }, 1000);
+const HandsSpell = ({ letter }) => {
+    const letterAsset = Assets[letter.toLowerCase()];
     return (
-        <Image style={Styles.HandImageStyle} source={currentLetter} />
+        <Image style={Styles.HandImageStyle} source={letterAsset} />
     );
 };
 
